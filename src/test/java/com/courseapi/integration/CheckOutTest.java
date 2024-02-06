abea27796f37c473c144948296f994f57ba746bf
LeandroSantosP <skp.tridimensional@gmail.com>
2024-02-06 10:54:04 -0300

    updated

--- src/test/java/com/courseapi/integration/CheckOutTest.java
+++ src/test/java/com/courseapi/integration/CheckOutTest.java
@@ -43,7 +43,6 @@ public class CheckOutTest {
             22222, "img-name-1.png", 5.0));
 
     stokeServiceRepository.save(new StokeEntryIn(UUID.randomUUID().toString(), courseId, 10));
-    stokeServiceRepository.count(courseId);
 
     CheckOutInput input = new CheckOutInput(courseId, "John Doe",
         "john.doe@gmail.com", "123456789");
@@ -62,6 +61,12 @@ public class CheckOutTest {
     assertEquals(getOrderOutput.email(), "john.doe@gmail.com");
     assertEquals(getOrderOutput.price(), 222.22);
     assertEquals(getOrderOutput.status(), "payed");
+
+    var stoke_entries = stokeServiceRepository.getAllByProductId(courseId);
+    int totalAfterPurchers = stoke_entries.stream()
+        .mapToInt(i -> "in".equals(i.getOperation()) ? i.getAmount() : -i.getAmount()).reduce(0,
+            Integer::sum);
+    assertEquals(totalAfterPurchers, 9);
   }
 
 }
